eb51eac7bf34dcc8bdac7d02b232a09fca42f7ea
AleexGleez12 <129044125+AleexGleez12@users.noreply.github.com>
2024-10-28 22:17:44 -0700

    .

--- Sopas/src/Nombre.java
+++
@@ -1,7 +0,0 @@
-public class Nombre {
-    public static void main(String[] args) {
-        String nombre = "Alejandro";
-        System.err.println("Bienvenido " + nombre);
-    }
-    
-}
--- Sopas/src/Personas.java
+++
@@ -1,7 +0,0 @@
-public class Personas {
-     public static void main(String[] args){
-        Persona ximena = new Persona();
-        System.out.println(ximena);
-         
-     }
-}
